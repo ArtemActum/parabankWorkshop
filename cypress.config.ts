fda5f5f41b3b2da15d015916edfdd431acfdac31
Artem <artem.mindsadyrov@actumdigital.com>
2024-07-22 23:34:25 +0200

    fix tests

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,11 +2,11 @@ import { defineConfig } from 'cypress';
 import * as createBundler from '@bahmutov/cypress-esbuild-preprocessor';
 import { addCucumberPreprocessorPlugin } from '@badeball/cypress-cucumber-preprocessor';
 import { createEsbuildPlugin } from '@badeball/cypress-cucumber-preprocessor/esbuild';
-import allureWriter from '@shelex/cypress-allure-plugin/writer';
+// import allureWriter from '@shelex/cypress-allure-plugin/writer';
 
 export default defineConfig({
   e2e: {
-    specPattern: '**/e2e/*.{feature,ts}',
+    specPattern: 'cypress/e2e/*.{feature,ts}',
     viewportWidth: 1920,
     viewportHeight: 1080,
     async setupNodeEvents(
@@ -24,17 +24,17 @@ export default defineConfig({
       );
 
       // Set up Allure plugin
-      allureWriter(on, config);
+      // allureWriter(on, config);
 
       // Make sure to return the config object as it might have been modified by the plugin.
       return config;
     },
-    reporter: 'cypress-allure-plugin',
-    reporterOptions: {
-      allure: {
-        resultsDir: 'allure-results',
-      },
-    },
+    // reporter: 'cypress-allure-plugin',
+    // reporterOptions: {
+    //   allure: {
+    //     resultsDir: 'allure-results',
+    //   },
+    // },
   },
 });
 
